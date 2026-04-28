31dbc0b4d8bf93cbaf9cdbf70afaa14fbef206b0
lucfl999 <juanmjerez56@gmail.com>
2026-04-28 17:50:19 -0300

    deploy: publish frontend to GitHub Pages

--- backend/prisma/migrations/20260425002546_add_referral_count_to_user_task/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "UserTask" ADD COLUMN     "linkOpenedAt" TIMESTAMP(3),
-ADD COLUMN     "referralCount" INTEGER NOT NULL DEFAULT 0;
--- backend/src/utils/logger.ts
+++
@@ -1,9 +0,0 @@
-export const logger = {
-  info: (...args: unknown[]) => {
-    if (process.env.NODE_ENV !== 'production') {
-      console.log('[Airdrop]', ...args);
-    }
-  },
-  warn: (...args: unknown[]) => console.warn('[Airdrop][WARN]', ...args),
-  error: (...args: unknown[]) => console.error('[Airdrop][ERROR]', ...args)
-};
